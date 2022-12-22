4a420c9dc766838d4b739721c3a2d239714fb392
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-12-22 11:25:54 +0900

    (draft) Create Language Server

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,83 @@
-import { languages } from "vscode";
+import { languages, window, workspace } from "vscode";
 import type { ExtensionContext } from "vscode";
+import { LanguageClient, TransportKind } from "vscode-languageclient/node";
+import type {
+  ServerOptions,
+  LanguageClientOptions,
+} from "vscode-languageclient/node";
+import { join } from "path";
 
 import * as Provider from "./ProviderCommon";
 import hoverProvider from "./hoverProvider";
 import definitionProvider from "./definitionProvider";
 import * as completionItemProvider from "./completionItemProvider";
+import updateDiagnosis from "./diagnosis";
 
-export function activate(context: ExtensionContext) {
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext): void {
+  const serverModule = context.asAbsolutePath(join("out", "server.js"));
+  const serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: { module: serverModule, transport: TransportKind.ipc },
+  };
+
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [
+      {
+        language: "virtual-turing-machine",
+        scheme: "file",
+      },
+    ],
+    synchronize: {
+      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
+    },
+  };
+
+  client = new LanguageClient(
+    "vtm-language-server",
+    "Virtual Turing Machine Language Server",
+    serverOptions,
+    clientOptions
+  );
+  client.start();
+
   console.log("Virtual Turing Machine Syntax is now active!");
-  context.subscriptions.push(
-    languages.registerHoverProvider(Provider.selector, hoverProvider)
-  );
-  context.subscriptions.push(
-    languages.registerDefinitionProvider(Provider.selector, definitionProvider)
-  );
-  context.subscriptions.push(
-    languages.registerCompletionItemProvider(
-      Provider.selector,
-      completionItemProvider.provider,
-      ...completionItemProvider.triggerCharacters
-    )
-  );
-}
+  // context.subscriptions.push(
+  //   languages.registerHoverProvider(Provider.selector, hoverProvider)
+  // );
+  // context.subscriptions.push(
+  //   languages.registerDefinitionProvider(Provider.selector, definitionProvider)
+  // );
+  // context.subscriptions.push(
+  //   languages.registerCompletionItemProvider(
+  //     Provider.selector,
+  //     completionItemProvider.provider,
+  //     ...completionItemProvider.triggerCharacters
+  //   )
+  // );
 
-export function deactivate() {}
+  // const diagnosicCollection = languages.createDiagnosticCollection(
+  //   "virtual-turing-machine-syntax"
+  // );
+  // if (window.activeTextEditor) {
+  //   updateDiagnosis(window.activeTextEditor.document, diagnosicCollection);
+  // }
+  // context.subscriptions.push(
+  //   window.onDidChangeActiveTextEditor((editor) => {
+  //     console.log("called!", editor);
+  //     if (editor) {
+  //       console.log("called2");
+  //       updateDiagnosis(editor.document, diagnosicCollection);
+  //     }
+  //   })
+  // );
+}
+// https://github.com/microsoft/vscode-extension-samples/blob/main/lsp-sample/client/src/extension.ts
+
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
